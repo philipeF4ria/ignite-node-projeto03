66c38bbe16f687b78894e3754d03bdf0f6da0057
Philipe Faria <philipefaria61@gmail.com>
2023-09-29 16:40:52 -0300

    test: Test the use case that get user profile

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,34 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(inMemoryUsersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await inMemoryUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: '123456',
+    })
+
+    const { user } = await sut.execute({ userId: createdUser.id })
+
+    expect(user.id).toEqual(createdUser.id)
+    expect(user.name).toEqual('John Doe')
+  })
+
+  it('should not be able to get user profile with invalid ID', async () => {
+    expect(() =>
+      sut.execute({ userId: 'non-existing-id' }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
